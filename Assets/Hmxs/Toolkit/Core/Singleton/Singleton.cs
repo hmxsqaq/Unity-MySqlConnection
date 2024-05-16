3dc539489e18a73f404d12eb69df676f3dbd021e
hmxs <hmxsqaq@163.com>
2024-05-16 14:01:56 +0800

    project init

---
+++ Assets/Hmxs/Toolkit/Core/Singleton/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Hmxs.Toolkit
+{
+    /// <summary>
+    /// 泛型单例基类
+    /// 采用Lazy进行实例化保证线程安全
+    /// </summary>
+    public abstract class Singleton<T> where T : Singleton<T>, new()
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance != null) return _instance;
+
+                _instance = new Lazy<T>(true).Value;
+                _instance.OnInstanceCreate(_instance);
+                return _instance;
+            }
+        }
+        
+        /// <summary>
+        /// 单例被第一次调用后调用该方法
+        /// </summary>
+        protected virtual void OnInstanceCreate(T instance) {}
+    }
+}
---
+++ Assets/Hmxs/Toolkit/Core/Singleton/SingletonMono.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Hmxs.Toolkit
+{
+    /// <summary>
+    /// 泛型单例基类-继承Mono
+    /// 采用Lazy进行实例化保证线程安全
+    /// </summary>
+    public abstract class SingletonMono<T> : MonoBehaviour where T : SingletonMono<T>
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance != null) return _instance;
+
+                _instance = FindObjectOfType<T>();
+                if (_instance != null) return _instance;
+
+                _instance = new Lazy<GameObject>(new GameObject(typeof(T).Name)).Value.AddComponent<T>();
+                _instance.OnInstanceCreate(_instance);
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// 单例被第一次调用后调用该方法
+        /// </summary>
+        protected virtual void OnInstanceCreate(T instance) {}
+    }
+}
